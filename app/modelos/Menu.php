23593071f560354d054ee5b32763c91c98cf172e
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2025-09-17 07:08:04 -0500

    Menú según el perfil en la barra lateral izquierda.

---
+++ app/modelos/Menu.php
@@ -0,0 +1,152 @@
+<?php
+class Menu
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Base;
+    }
+
+    public function listarMenusNivel1($id_perfil)
+    {
+        $this->db->query("SELECT m.*
+                            FROM sw_menu m,
+                                 sw_menu_perfil mp 
+                           WHERE m.id_menu = mp.id_menu
+                             AND mp.id_perfil = $id_perfil 
+                             AND mnu_padre = 0
+                             AND mnu_publicado = 1
+                        ORDER BY mnu_orden");
+
+        return $this->db->registros();
+    }
+
+    public function listarMenusHijos($mnu_padre)
+    {
+        $this->db->query("SELECT *
+                            FROM sw_menu
+                           WHERE mnu_padre = $mnu_padre
+                             AND mnu_publicado = 1
+                        ORDER BY mnu_orden");
+
+        return $this->db->registros();
+    }
+
+    public function obtenerMenusPadres()
+    {
+        $this->db->query("SELECT m.*, pe_nombre FROM `sw_menu` m, `sw_perfil` p, `sw_menu_perfil` mp  WHERE p.id_perfil = mp.id_perfil AND m.id_menu = mp.id_menu AND mnu_padre = 0 ORDER BY pe_nombre, mnu_padre, mnu_orden");
+
+        return $this->db->registros();
+    }
+
+    public function obtenerMenusHijos($mnu_padre)
+    {
+        $this->db->query("SELECT *
+                            FROM sw_menu
+                           WHERE mnu_padre = $mnu_padre
+                        ORDER BY mnu_orden");
+
+        return $this->db->registros();
+    }
+
+    public function insertarMenu($datos)
+    {
+        $this->db->query('INSERT INTO sw_menu (mnu_texto, mnu_link, mnu_publicado, mnu_icono, id_perfil) VALUES (:mnu_texto, :mnu_link, :mnu_publicado, :mnu_icono, :id_perfil)');
+
+        //Vincular valores
+        $this->db->bind(':mnu_texto', $datos['mnu_texto']);
+        $this->db->bind(':mnu_link', $datos['mnu_link']);
+        $this->db->bind(':mnu_publicado', $datos['mnu_publicado']);
+        $this->db->bind(':mnu_icono', $datos['mnu_icono']);
+        $this->db->bind(':id_perfil', $datos['id_perfil']);
+
+        $this->db->execute();
+
+        $this->db->query("SELECT MAX(id_menu) AS lastInsertId FROM sw_menu");
+        $lastInsertId = $this->db->registro()->lastInsertId;
+
+        $this->db->query('INSERT INTO sw_menu_perfil (id_perfil, id_menu) VALUES (:id_perfil, :id_menu)');
+
+        //Vincular valores
+        $this->db->bind(':id_perfil', $datos['id_perfil']);
+        $this->db->bind(':id_menu', $lastInsertId);
+
+        $this->db->execute();
+    }
+
+    public function obtenerMenuPorId($id)
+    {
+        $this->db->query("SELECT m.*, mp.id_perfil FROM `sw_menu` m, `sw_menu_perfil` mp WHERE m.id_menu = mp.id_menu AND m.id_menu = :id_menu");
+        $this->db->bind(':id_menu', $id);
+
+        return $this->db->registro();
+    }
+
+    public function actualizarMenu($datos)
+    {
+        $this->db->query('UPDATE sw_menu SET mnu_texto = :mnu_texto, mnu_link = :mnu_link, mnu_publicado = :mnu_publicado, mnu_icono = :mnu_icono WHERE id_menu = :id_menu');
+
+        //Vincular valores
+        $this->db->bind(':mnu_texto', $datos['mnu_texto']);
+        $this->db->bind(':mnu_link', $datos['mnu_link']);
+        $this->db->bind(':mnu_publicado', $datos['mnu_publicado']);
+        $this->db->bind(':mnu_icono', $datos['mnu_icono']);
+        $this->db->bind(':id_menu', $datos['id_menu']);
+
+        $this->db->execute();
+
+        $this->db->query('UPDATE sw_menu_perfil SET id_perfil = :id_perfil WHERE id_menu = :id_menu');
+
+        //Vincular valores
+        $this->db->bind(':id_perfil', $datos['id_perfil']);
+        $this->db->bind(':id_menu', $datos['id_menu']);
+
+        $this->db->execute();
+    }
+
+    public function eliminarMenu($id)
+    {
+        $this->listarMenusHijos($id);
+
+        if ($this->db->rowCount() == 0) {
+            $this->db->query("DELETE FROM sw_menu WHERE id_menu = :id_menu");
+
+            //Vincular valores
+            $this->db->bind(':id_menu', $id);
+
+            $this->db->execute();
+        }
+    }
+
+    public function guardarOrden($menus)
+    {
+        foreach ($menus as $var => $value) {
+            // $this->where('id', $value->id)->update(['menu_id' => 0, 'orden' => $var + 1]);
+            $this->db->query('UPDATE sw_menu SET mnu_padre = :mnu_padre, mnu_orden = :mnu_orden, mnu_nivel = :mnu_nivel WHERE id_menu = :id_menu');
+            //Vincular valores
+            $this->db->bind(':mnu_padre', 0);
+            $this->db->bind(':mnu_nivel', 1);
+            $this->db->bind(':mnu_orden', $var + 1);
+            $this->db->bind(':id_menu', $value['id']);
+            //Ejecutar la sentencia UPDATE
+            $this->db->execute();
+            if (!empty($value['children'])) {
+                foreach ($value['children'] as $key => $vchild) {
+                    $update_id = $vchild['id'];
+                    $parent_id = $value['id'];
+                    // $this->where('id', $update_id)->update(['menu_id' => $parent_id, 'orden' => $key + 1]);
+                    $this->db->query('UPDATE sw_menu SET mnu_padre = :mnu_padre, mnu_orden = :mnu_orden, mnu_nivel = :mnu_nivel WHERE id_menu = :id_menu');
+                    //Vincular valores
+                    $this->db->bind(':mnu_padre', $parent_id);
+                    $this->db->bind(':mnu_nivel', 2);
+                    $this->db->bind(':mnu_orden', $key + 1);
+                    $this->db->bind(':id_menu', $update_id);
+                    //Ejecutar la sentencia UPDATE
+                    $this->db->execute();
+                }
+            }
+        }
+        return count($menus);
+    }
+}
--- app/vistas/layouts/aside.php
+++ app/vistas/layouts/aside.php
@@ -11,8 +11,42 @@
                     <div class="sb-nav-link-icon"><i class="fas fa-home"></i></div>
                     Dashboard
                 </a>
-                
-
+                <?php
+                $menuModelo = $this->modelo('Menu');
+                $menusNivel1 = $menuModelo->listarMenusNivel1($_SESSION['id_perfil']);
+                foreach ($menusNivel1 as $menu) {
+                ?>
+                    <?php if (count($menuModelo->listarMenusHijos($menu->id_menu)) > 0) { ?>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapse<?= $menu->mnu_texto ?>" aria-expanded="false" aria-controls="collapse<?= $menu->mnu_texto ?>">
+                            <div class="sb-nav-link-icon"><i class="fas <?= $menu->mnu_icono == '' ? 'fa-link' : $menu->mnu_icono  ?>"></i></div>
+                            <?= $menu->mnu_texto ?>
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="collapse<?= $menu->mnu_texto ?>" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <?php
+                                foreach ($menuModelo->listarMenusHijos($menu->id_menu) as $menu2) {
+                                    $uriSegments = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+                                    $active = ($uriSegments[2] == $menu2->mnu_link) ? 'active' : '';
+                                    $icono = "";
+                                    if ($menu2->mnu_icono != "") {
+                                        $icono = "<i class='fa fa-fw $menu2->mnu_icono'></i> ";
+                                    }
+                                ?>
+                                    <a class="nav-link" href="<?php echo RUTA_URL . '/' . $menu2->mnu_link; ?>"><?= $menu2->mnu_texto; ?></a>
+                                <?php } ?>
+                            </nav>
+                        </div>
+                    <?php } else {
+                        $uriSegments = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+                        $active = ($uriSegments[2] == $menu->mnu_link) ? 'active' : '';
+                    ?>
+                        <a class="nav-link" href="<?= RUTA_URL . '/' . $menu->mnu_link; ?>">
+                            <div class="sb-nav-link-icon"><i class="fas <?= $menu->mnu_icono == '' ? 'fa-link' : $menu->mnu_icono  ?>"></i></div>
+                            <?= $menu->mnu_texto; ?>
+                        </a>
+                    <?php } ?>
+                <?php } ?>
             </div>
         </div>
     </nav>
