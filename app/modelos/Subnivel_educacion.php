a6bfd61cc22128ebc617112d9082a2798a5a9bd0
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2025-10-23 15:01:00 -0500

    Cambio de nombre de sw_nivel_educacion a sw_sub_nivel_educacion.

--- app/modelos/Subnivel_educacion.php
+++ app/modelos/Subnivel_educacion.php
@@ -10,19 +10,19 @@ class Subnivel_educacion
 
     public function obtenerTodos($institucion_id)
     {
-        $this->db->query("SELECT * FROM sw_nivel_educacion WHERE institucion_id = $institucion_id ORDER BY orden ASC");
+        $this->db->query("SELECT * FROM sw_sub_nivel_educacion WHERE institucion_id = $institucion_id ORDER BY orden ASC");
         return $this->db->registros();
     }
 
     public function obtenerSubnivel($id)
     {
-        $this->db->query("SELECT * FROM sw_nivel_educacion WHERE id_nivel_educacion = $id");
+        $this->db->query("SELECT * FROM sw_sub_nivel_educacion WHERE id_nivel_educacion = $id");
         return $this->db->registro();
     }
 
     public function existeNombre($nombre)
     {
-        $this->db->query("SELECT * FROM sw_nivel_educacion WHERE nombre = '$nombre'");
+        $this->db->query("SELECT * FROM sw_sub_nivel_educacion WHERE nombre = '$nombre'");
         $subnivel = $this->db->registro();
 
         return !empty($subnivel);
@@ -30,11 +30,11 @@ class Subnivel_educacion
 
     public function insertar($datos)
     {
-        $this->db->query('SELECT MAX(orden) AS max_orden FROM sw_nivel_educacion');
+        $this->db->query('SELECT MAX(orden) AS max_orden FROM sw_sub_nivel_educacion');
         $registro = $this->db->registro();
         $max_orden = (!empty($registro)) ? $registro->max_orden + 1 : 1;
 
-        $this->db->query('INSERT INTO sw_nivel_educacion (institucion_id, nombre, es_bachillerato, orden) VALUES (:institucion_id, :nombre, :es_bachillerato, :orden)');
+        $this->db->query('INSERT INTO sw_sub_nivel_educacion (institucion_id, nombre, es_bachillerato, orden) VALUES (:institucion_id, :nombre, :es_bachillerato, :orden)');
 
         //Vincular valores
         $this->db->bind(':institucion_id', $datos['institucion_id']);
@@ -47,7 +47,7 @@ class Subnivel_educacion
 
     public function actualizar($datos)
     {
-        $this->db->query('UPDATE sw_nivel_educacion SET nombre = :nombre, es_bachillerato = :es_bachillerato WHERE id_nivel_educacion = :id_nivel_educacion');
+        $this->db->query('UPDATE sw_sub_nivel_educacion SET nombre = :nombre, es_bachillerato = :es_bachillerato WHERE id_nivel_educacion = :id_nivel_educacion');
 
         //Vincular valores
         $this->db->bind(':nombre', $datos['nombre']);
@@ -59,7 +59,7 @@ class Subnivel_educacion
 
     public function eliminar($id)
     {
-        $this->db->query('DELETE FROM `sw_nivel_educacion` WHERE `id_nivel_educacion` = :id_nivel_educacion');
+        $this->db->query('DELETE FROM `sw_sub_nivel_educacion` WHERE `id_nivel_educacion` = :id_nivel_educacion');
 
         //Vincular valores
         $this->db->bind(':id_nivel_educacion', $id);
